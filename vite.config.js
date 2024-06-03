f1cab4df13794b1ae22f06a99169b885f6684bd4
Aditya Shibu <as2397@hw.ac.uk>
2024-06-03 23:03:05 +0400

    Updated packages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal_portfolio/",
+  base: "/personal_portfolio_deploy/",
   plugins: [react()],
 })
