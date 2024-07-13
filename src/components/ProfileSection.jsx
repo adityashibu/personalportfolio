492ec4e146962ee48e755d0fa1913a53a87d8e81
Aditya Shibu <as2397@hw.ac.uk>
2024-07-13 21:22:54 +0400

    Added animations

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { personalpicture, resume, vcfCard } from '../assets';
 import { SectionWrapper } from '../hoc';
 
+import { motion } from 'framer-motion';
+import { fadeIn, textVariant } from '../utils/motion';
+
 const ProfileSection = () => {
     return (
-        <div className="container mx-auto flex flex-col items-center sm:items-start">
+        <motion.div variants={fadeIn("", "", 0.3)} className="container mx-auto flex flex-col items-center sm:items-start">
             <div className="grid grid-cols-1 sm:grid-cols-4 gap-6 items-center w-full max-w-4xl">
                 <div className="flex justify-center sm:justify-start col-span-1 -mt-10">
                     <div className="w-45 h-45 sm:w-70 sm:h-70 rounded-full overflow-hidden border-4 border-white">
@@ -15,7 +18,7 @@ const ProfileSection = () => {
                     <h1 className="text-white text-3xl sm:text-5xl font-bold">Aditya S</h1>
                     <p className="text-gray-400 text-lg sm:text-2xl mt-2">Student Developer</p>
                 </div>
-                <div className="col-span-1 text-center sm:text-left sm:-mt-10 mt-10">
+                <motion.div variants={textVariant()} className="col-span-1 text-center sm:text-left sm:-mt-10 mt-10">
                     <h2 className="text-white text-2xl sm:text-3xl font-bold mb-4">Download</h2>
                     <div className="flex flex-col space-y-4">
                         <a href={vcfCard} download className="bg-white hover:bg-black text-black hover:text-white font-bold py-2 px-4 rounded glow-on-hover">
@@ -25,10 +28,10 @@ const ProfileSection = () => {
                             My Resume
                         </a>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
-        </div>
+        </motion.div>
     );
 }
 
