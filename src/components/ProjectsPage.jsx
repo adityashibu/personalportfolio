5c1fb92f35b78a79542cd5b84b11b7355dbfd859
Aditya Shibu <as2397@hw.ac.uk>
2024-07-14 22:15:46 +0400

    Updated # to @

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -45,7 +45,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                 <div className="mt-4 flex flex-wrap gap-2">
                     {tags.map((tag) => (
                         <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-                            #{tag.name}
+                            @{tag.name}
                         </p>
                     ))}
                 </div>
