1429a87e96568e6da455992588214db9f5a4bbf6
Aditya Shibu <as2397@hw.ac.uk>
2024-08-03 17:56:24 +0400

    Added slider component

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect } from 'react';
-import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
+import Slider from "react-slick";
 
 import { styles } from '../styles';
 import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const CustomArrow = ({ className, style, onClick, icon }) => {
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: 'none' }} // Hide arrows
+            onClick={onClick}
+        >
+            {icon}
+        </div>
+    );
+};
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
     return (
         <motion.div
             variants={fadeIn("up", "spring", index * 0.5, 0.75)}
         >
-            <Tilt
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450
-                }}
-                className="bg-[#FAF9F6] p-5 rounded-2xl sm:w-[360px] w-full"
-            >
+            <div className="bg-[#FAF9F6] p-5 rounded-2xl sm:w-[360px] w-full project-card">
                 <div className="relative w-full h-[230px]">
                     <img
                         src={image}
@@ -49,15 +56,57 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                         </p>
                     ))}
                 </div>
-            </Tilt>
+            </div>
         </motion.div>
-    )
+    );
 }
 
 const ProjectsPage = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false, // Hide arrows
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '0',
+                    infinite: true,
+                    dots: true,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '0',
+                    initialSlide: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                }
+            }
+        ]
+    };
 
     return (
         <>
@@ -74,17 +123,17 @@ const ProjectsPage = () => {
                     variants={fadeIn("", "", 0.1)}
                     className="mt-3 text-gray-300 text-[17px] max-w-3xl leading-[30px] "
                 >
-                    Welcome to my developer journey! I'm a student constantly learning and expanding my skillset. Here you'll find a collection of my projects – each one a stepping stone in my growth as a developer.  From Python automation scripts to Website development using React and Vite (Technologies I've used for developing this website), you can see how I've honed my abilities in various Technologies. Moreover I'm eager to learn about newer and upcoming technologies to sharpen and keep updating myself.
+                    Welcome to my developer journey! I'm a student constantly learning and expanding my skillset. Here you'll find a collection of my projects – each one a stepping stone in my growth as a developer. From Python automation scripts to Website development using React and Vite (Technologies I've used for developing this website), you can see how I've honed my abilities in various Technologies. Moreover I'm eager to learn about newer and upcoming technologies to sharpen and keep updating myself.
                 </motion.p>
             </div>
 
-            <div className="mt-20 flex flex-wrap gap-7 justify-center">
+            <Slider {...settings} className="mt-20 flex justify-center">
                 {projects.map((project, index) => (
                     <ProjectCard key={`project-${index}`} index={index} {...project} />
                 ))}
-            </div>
+            </Slider>
         </>
-    )
+    );
 }
 
 export default SectionWrapper(ProjectsPage, "ProjectsPage");
